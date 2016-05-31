fae053642d46ff216cbb0952e5ed39afe9933aa7
devmobmatmut <devmobmatmut@gmail.com>
2016-05-31 15:27:40 +0200

    Fix method check

--- src/ios/startApp.m
+++ src/ios/startApp.m
@@ -7,7 +7,7 @@
     
     CDVPluginResult* pluginResult = nil;
     
-    NSString* scheme = [command.arguments objectAtIndex:0];
+    NSString* scheme = [[command.arguments objectAtIndex:0] objectForKey:@"application"];
     
     if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:scheme]]) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:(true)];
